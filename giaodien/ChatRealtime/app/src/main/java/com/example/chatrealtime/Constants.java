b23e26c835e7238576c6d685bcdd31346216a609
Ho Phuoc Loi <hophuocloi108@gmail.com>
2026-03-24 07:41:19 +0700

    24/03 Hoan thanh

--- giaodien/ChatRealtime/app/src/main/java/com/example/chatrealtime/Constants.java
+++ giaodien/ChatRealtime/app/src/main/java/com/example/chatrealtime/Constants.java
@@ -2,6 +2,6 @@ package com.example.chatrealtime;
 
 public class Constants {
     public static final String BASE_URL = "http://103.57.221.189:8080/api/";
-    public static final String WEBSOCKET_URL = "ws://103.57.221.189:8080/ws";
+    public static final String WEBSOCKET_URL = "http://103.57.221.189:8080/ws";
     public static final String IMAGE_BASE_URL = "http://103.57.221.189:8080";
 }
